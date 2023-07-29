a01b8d4c5555933dce4ac8a0729be5b498248e8a
Bartosz Pindel <bartos.pindel@gmail.com>
2023-07-29 14:42:39 +0200

    some fixes, and improvements

--- scripts/showAllDishes.js
+++ scripts/showAllDishes.js
@@ -1,17 +1,25 @@
 const btnShowDishes = document.querySelector(".btn.showDishes");
 const containerAllDishes = document.querySelector(".allDishes");
+const mainSec = document.querySelector(".mainSec.allDishes");
 
 btnShowDishes.addEventListener("click", () => {
-  if (containerAllDishes.innerHTML.length > 0) return;
+  if (containerAllDishes) {
+    if (containerAllDishes.innerHTML.length > 0) return false;
+    mainSec.classList.toggle("noActive");
+    mainSec.innerHTML += `<ul class="mainSec dishesList"></ul>`;
 
-  dishes.forEach(({ name, price, description }) => {
-    containerAllDishes.innerHTML += `<li>
-        <p>
-          ${name} at ${price} zł
-        </p>
-        <p>${description}</p>
+    const dishesList = document.querySelector(".dishesList");
+
+    dishes.forEach(({ name, price, description }) => {
+      dishesList.innerHTML += `<li>
+      <p>
+      ${name} at ${price} zł
+      </p>
+      <p>${description}</p>
       </li>`;
-  });
+    });
+  } else {
+  }
   // btnShowDishes.classList.toggle("active");
   // btnShowDishes.setAttribute("disabled", true);
 });
