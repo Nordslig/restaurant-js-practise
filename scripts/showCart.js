12a11e822c5deeca4f8cb4f612f1815e7e12711c
Bartosz Pindel <bartos.pindel@gmail.com>
2023-08-09 19:40:26 +0200

    shows cart content

--- scripts/showCart.js
+++ scripts/showCart.js
@@ -1,3 +1,40 @@
-const cart = [];
+const cartList = document.querySelector(".cartList");
+const divCart = document.querySelector("div.cart");
 
-export default { cart };
+console.log(cart);
+
+const showCart = () => {
+  if (cart.length > 0) {
+    cart.forEach((item) => {
+      const { id, dName, dPrice } = item;
+
+      const li = document.createElement("li");
+      const pName = document.createElement("p");
+      pName.textContent = dName;
+      const pPrice = document.createElement("p");
+      pPrice.textContent = dPrice;
+
+      li.append(pName, pPrice);
+      cartList.append(li);
+      // console.log(id, dName, dPrice);
+    });
+  } else {
+    const p = document.createElement("p");
+    p.textContent = "You have no items in cart yet.";
+    divCart.append(p);
+  }
+
+  // cart.forEach((item) => {
+  //   const { id, dName, dPrice } = item;
+
+  //   const li = document.createElement("li");
+  //   const pName = document.createElement("p");
+  //   p.textContent = dName
+  //   const pPrice = document.createElement("p");
+  //   pPrice.textContent = dPrice
+
+  //   li.append(pName, pPrice);
+  //       cartList.append(li);
+  //   // console.log(id, dName, dPrice);
+  // });
+};
