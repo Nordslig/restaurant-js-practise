b4c092b11ac8f4c7c764a4e73b07d95e7a3c12d0
Bartosz Pindel <bartos.pindel@gmail.com>
2023-08-09 19:40:36 +0200

    adds item to cart from menu

--- scripts/addItemToCart.js
+++ scripts/addItemToCart.js
@@ -4,10 +4,10 @@ const addBtns = document.querySelectorAll(".menu .btn");
 
 addBtns.forEach((btn) =>
   btn.addEventListener("click", () => {
-    console.log(btn.closest(`.btn `));
-    // const dName = btn.closest(".dishName").textContent;
-    // const dPrice = btn.closest(".dishPrice").textContent;
-    cart.push({ dName, dPrice });
+    const id = Math.round(Math.random() * 100);
+    const dName = btn.closest(`li`).firstElementChild.children[0].textContent;
+    const dPrice = btn.closest(`li`).firstElementChild.children[1].textContent;
+    cart.push({ id, dName, dPrice });
 
     console.log(cart);
   })
