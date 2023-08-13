3415506afef735f433a2e0f842162b0595efde70
Bartosz Pindel <bartos.pindel@gmail.com>
2023-08-13 22:35:29 +0200

    shows cart with quantities on items, clean up and code reformat

--- scripts/showCart.js
+++ scripts/showCart.js
@@ -1,40 +1,31 @@
 const cartList = document.querySelector(".cartList");
 const divCart = document.querySelector("div.cart");
 
-console.log(cart);
-
 const showCart = () => {
   if (cart.length > 0) {
+    cartList.innerHTML = "";
+
     cart.forEach((item) => {
-      const { id, dName, dPrice } = item;
+      const { dishName, dishPrice, quantity } = item;
 
       const li = document.createElement("li");
-      const pName = document.createElement("p");
-      pName.textContent = dName;
-      const pPrice = document.createElement("p");
-      pPrice.textContent = dPrice;
 
-      li.append(pName, pPrice);
+      const itemName = document.createElement("p");
+      itemName.textContent = dishName;
+
+      const itemPrice = document.createElement("p");
+      itemPrice.textContent = dishPrice;
+
+      const itemQuantity = document.createElement("p");
+      itemQuantity.textContent = quantity;
+
+      li.append(itemName, itemPrice, itemQuantity);
       cartList.append(li);
-      // console.log(id, dName, dPrice);
     });
   } else {
     const p = document.createElement("p");
     p.textContent = "You have no items in cart yet.";
+
     divCart.append(p);
   }
-
-  // cart.forEach((item) => {
-  //   const { id, dName, dPrice } = item;
-
-  //   const li = document.createElement("li");
-  //   const pName = document.createElement("p");
-  //   p.textContent = dName
-  //   const pPrice = document.createElement("p");
-  //   pPrice.textContent = dPrice
-
-  //   li.append(pName, pPrice);
-  //       cartList.append(li);
-  //   // console.log(id, dName, dPrice);
-  // });
 };
