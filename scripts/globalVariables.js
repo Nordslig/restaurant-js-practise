5e2cd07c835a9d1ecf9a0bdfc7d57c3d76b7a33c
Bartosz Pindel <bartos.pindel@gmail.com>
2023-08-15 20:23:03 +0200

    add sides and drinks arrays

--- scripts/globalVariables.js
+++ scripts/globalVariables.js
@@ -3,17 +3,63 @@ const dishes = [
     id: "d1",
     name: "Kiełba",
     price: 10,
+    type: "grill",
   },
   {
     id: "d2",
     name: "Kiełba better",
     price: 15,
+    type: "grill",
   },
   {
     id: "d3",
     name: "Kiełba the best",
     price: 20,
+    type: "grill",
+  },
+  {
+    id: "d4",
+    name: "Pizza",
+    price: 40,
+    type: "dish",
   },
 ];
+
+const sides = {
+  sauces: [
+    {
+      name: "Tatar",
+      price: 3,
+    },
+    {
+      name: "Garlic",
+      price: 3,
+    },
+    {
+      name: "Honey-mustard",
+      price: 5,
+    },
+    {
+      name: "Cheese",
+      price: 5,
+    },
+  ],
+  sideDishes: [
+    { name: "Chips", price: 8 },
+    { name: "Rice", price: 6 },
+  ],
+};
+
+const drinks = {
+  soda: [
+    { name: "Pepsi", price: 10 },
+    { name: "Cola", price: 10 },
+    { name: "Coke", price: 500 },
+  ],
+  alcohol: [
+    { name: "Beer", price: 6 },
+    { name: "Trans Beer", price: 9 },
+  ],
+};
 
 const cart = [];
